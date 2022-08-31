514c189fa7bb5ba18827ebea90a71c79a2a7db36
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-08-31 21:49:13 +0200

    Add topic "Standard input"

---
+++ Topics/Standard input/Four numbers/src/Main.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val i1 = scanner.nextInt()
+    val i2 = scanner.nextInt()
+    val i3 = scanner.nextInt()
+    val i4 = scanner.nextInt()
+    println(i1)
+    println(i2)
+    println(i3)
+    println(i4)
+}

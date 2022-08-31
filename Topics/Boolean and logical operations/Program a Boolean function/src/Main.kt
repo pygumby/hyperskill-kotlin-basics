3b7b26ad2cc5513b435eadf52458ebdbb257edc6
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-08-31 22:16:25 +0200

    Add topic "Boolean and logical operations"

---
+++ Topics/Boolean and logical operations/Program a Boolean function/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val x = readLine().toBoolean() // read other values in the same way
+    // write your code here
+    val y = readln().toBoolean()
+    val z = readln().toBoolean()
+    println(!(x && y) || z)
+}

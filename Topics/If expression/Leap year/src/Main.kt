3851d516eed958adb3f7de48d0b62e5b69ea08ee
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-09-01 21:52:54 +0200

    Add topic "If expression"

---
+++ Topics/If expression/Leap year/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val year = readln().toInt()
+
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        println("Leap")
+    } else {
+        println("Regular")
+    }
+}
---
+++ Topics/If expression/Odd or even/src/Main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val n = readln().toInt()
+    println(if (n % 2 == 0) "EVEN" else "ODD")
+}

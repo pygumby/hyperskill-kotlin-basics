bf9dab8503f6b522127991728339c7b8b53a46a5
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-09-05 18:09:05 +0200

    Add topic "String arrays"

---
+++ Topics/String arrays/Guard On The Gate/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var backFromTheWall = readLine()!!.split(',').map { it }.toTypedArray()
+    val returnedWatchman = readLine()!!.toString()  
+    // do not touch the lines above
+    // write your code here
+    println((backFromTheWall + returnedWatchman).joinToString())
+}

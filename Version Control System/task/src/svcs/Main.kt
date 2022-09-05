68a31093746c919a3323ba6567a1dc36cc8b26d9
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-09-05 19:27:09 +0200

    Add stage 1/4 "Help page"

--- Version Control System/task/src/svcs/Main.kt
+++ Version Control System/task/src/svcs/Main.kt
@@ -1,5 +1,30 @@
 package svcs
 
 fun main(args: Array<String>) {
+    val helpConfig = "Get and set a username."
+    val helpAdd = "Add a file to the index."
+    val helpLog = "Show commit logs."
+    val helpCommit = "Save changes."
+    val helpCheckout = "Restore a file."
 
+    fun printHelpAll() = println("""
+        These are SVCS commands:
+        config     $helpConfig
+        add        $helpAdd
+        log       $helpLog
+        commit     $helpCommit
+        checkout   $helpCheckout
+    """.trimIndent())
+
+    if (args.isEmpty()) return printHelpAll()
+
+    when (val arg = args.first()) {
+        "--help"   -> printHelpAll()
+        "config"   -> println(helpConfig)
+        "add"      -> println(helpAdd)
+        "log"      -> println(helpLog)
+        "commit"   -> println(helpCommit)
+        "checkout" -> println(helpCheckout)
+        else       -> println("'$arg' is not a SVCS command.")
+    }
 }

17a45d839beb0af7233dcf3010a2b58cbd16a169
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-09-11 16:49:34 +0200

    Add stage 2/4 "Add & config"

--- Version Control System/task/src/svcs/Main.kt
+++ Version Control System/task/src/svcs/Main.kt
@@ -1,30 +1,113 @@
 package svcs
 
-fun main(args: Array<String>) {
-    val helpConfig = "Get and set a username."
-    val helpAdd = "Add a file to the index."
-    val helpLog = "Show commit logs."
-    val helpCommit = "Save changes."
-    val helpCheckout = "Restore a file."
+import java.io.File
 
-    fun printHelpAll() = println("""
+fun help(arg: String = "--help"): Unit {
+    val config = "Get and set a username."
+    val add = "Add a file to the index."
+    val log = "Show commit logs."
+    val commit = "Save changes."
+    val checkout = "Restore a file."
+    val all = """
         These are SVCS commands:
-        config     $helpConfig
-        add        $helpAdd
-        log       $helpLog
-        commit     $helpCommit
-        checkout   $helpCheckout
-    """.trimIndent())
+        config     $config
+        add        $add
+        log        $log
+        commit     $commit
+        checkout   $checkout
+    """.trimIndent()
 
-    if (args.isEmpty()) return printHelpAll()
+    when (arg) {
+        "config" -> println(config)
+        "add" -> println(add)
+        "log" -> println(log)
+        "commit" -> println(commit)
+        "checkout" -> println(checkout)
+        "--help" -> println(all)
+    }
+}
 
-    when (val arg = args.first()) {
-        "--help"   -> printHelpAll()
-        "config"   -> println(helpConfig)
-        "add"      -> println(helpAdd)
-        "log"      -> println(helpLog)
-        "commit"   -> println(helpCommit)
-        "checkout" -> println(helpCheckout)
-        else       -> println("'$arg' is not a SVCS command.")
+enum class VcsFile {
+    VCS_DIR,
+    CONFIG_TXT,
+    INDEX_TXT
+}
+
+fun getVcsFile(vcsFile: VcsFile): File {
+    val s = File.separator
+    return when (vcsFile) {
+        VcsFile.VCS_DIR -> File("vcs")
+        VcsFile.CONFIG_TXT -> File("vcs${File.separator}config.txt")
+        VcsFile.INDEX_TXT -> File("vcs${File.separator}index.txt")
+    }
+}
+
+fun validateVcsDir() {
+    val vcsDir = getVcsFile(VcsFile.VCS_DIR)
+    val configFile = getVcsFile(VcsFile.CONFIG_TXT)
+    val indexFile = getVcsFile(VcsFile.INDEX_TXT)
+
+    if (vcsDir.exists()) {
+        if (!configFile.exists()) configFile.createNewFile()
+        if (!indexFile.exists()) indexFile.createNewFile()
+    } else {
+        vcsDir.mkdir()
+        configFile.createNewFile()
+        indexFile.createNewFile()
+    }
+}
+
+fun config(userName: String?): Unit {
+    validateVcsDir()
+
+    val configFile = getVcsFile(VcsFile.CONFIG_TXT)
+
+    if (userName == null) {
+        val configFileText = configFile.readText()
+
+        if (configFileText.isEmpty()) {
+            println("Please, tell me who you are.")
+        } else {
+            println("The username is $configFileText.")
+        }
+    } else {
+        configFile.writeText(userName)
+        println("The username is ${configFile.readText()}.")
+    }
+}
+
+fun add(fileName: String?): Unit {
+    validateVcsDir()
+
+    val indexFile = getVcsFile(VcsFile.INDEX_TXT)
+
+    if (fileName == null) {
+        val indexFileText = indexFile.readText()
+
+        if (indexFileText.isEmpty()) {
+            println("Add a file to the index.")
+        } else {
+            println("Tracked files:\n$indexFileText")
+        }
+    } else {
+        if (File(fileName).exists()) {
+            indexFile.appendText("$fileName\n")
+            println("The file '$fileName' is tracked.")
+        } else {
+            println("Can't find '$fileName'.")
+        }
+    }
+}
+
+fun main(args: Array<String>): Unit {
+    if (args.isEmpty()) return help()
+    when (args[0]) {
+        "config" -> config(args.elementAtOrNull(1))
+        "add" -> add(args.elementAtOrNull(1))
+        "log" -> help(args[0])
+        "commit" -> help(args[0])
+        "checkout" -> help(args[0])
+        "--help" -> help(args[0])
+        else -> println("'${args[0]}' is not a SVCS command.")
     }
 }

cebfd199be885d536e6dbfc61694825915d81fee
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-09-19 21:16:56 +0200

    Add stage 1/5 of "Indigo Card Game"

--- Indigo Card Game/Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/Indigo Card Game/task/src/indigo/Main.kt
@@ -1,5 +1,11 @@
 package indigo
 
+val RANKS = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+val SUITS = listOf("♦", "♥", "♠", "♣")
+val DECK = RANKS.map { rank ->  SUITS.map { suit -> rank + suit } }.flatten()
+
 fun main() {
-    println("Hello World!")
+    println(RANKS.joinToString(" "))
+    println(SUITS.joinToString(" "))
+    println(DECK.joinToString(" "))
 }

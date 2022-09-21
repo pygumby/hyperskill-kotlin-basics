1470a59a38c674e4c91493bec46a5231559d0c82
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-09-21 21:04:20 +0200

    Add topic "Member functions"

---
+++ Indigo Card Game/Topics/Member functions/Arithmetic operations/src/Main.kt
@@ -0,0 +1,7 @@
+// write your class here
+class ArithmeticOperations(val x: Int, val y: Int) {
+    fun addition() = this.x + this.y
+    fun subtraction() = this.x - this.y
+    fun multiplication() = this.x * this.y
+    fun division() = this.x / this.y
+}

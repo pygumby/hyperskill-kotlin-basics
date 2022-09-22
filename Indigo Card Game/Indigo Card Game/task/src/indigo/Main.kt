febc9f99c5f57046143814cf5101d0a50c2b5881
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-09-22 23:46:31 +0200

    Add stage 2/5 of "Indigo Card Game"

--- Indigo Card Game/Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/Indigo Card Game/task/src/indigo/Main.kt
@@ -1,11 +1,83 @@
 package indigo
 
-val RANKS = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
-val SUITS = listOf("♦", "♥", "♠", "♣")
-val DECK = RANKS.map { rank ->  SUITS.map { suit -> rank + suit } }.flatten()
+enum class Rank(val symbol: String) {
+    ACE("A"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    JACK("J"),
+    QUEEN("Q"),
+    KING("K"),
+}
+
+enum class Suit(val symbol: String) {
+    HEARTS("♥"),
+    CLUBS("♣"),
+    SPADES("♠"),
+    DIAMONDS("♦"),
+}
+
+class Card(private val rank: Rank, private val suit: Suit) {
+    override fun toString(): String = "${rank.symbol}${suit.symbol}"
+}
+
+class Deck {
+    private var cards = listOf<Card>()
+
+    fun reset() {
+        this.cards = Rank.values()
+            .map { rank -> Suit.values().map { Card(rank, it) } }
+            .flatten()
+        println("Card deck is reset.")
+    }
+
+    fun shuffle() {
+        this.cards = this.cards.shuffled()
+        println("Card deck is shuffled.")
+    }
+
+    fun get(numberOfCards: String) {
+        when (val n = numberOfCards.toIntOrNull()) {
+            null -> return println("Invalid number of cards.")
+            !in 1 .. 52 -> return println("Invalid number of cards.")
+            !in 1 .. this.cards.size -> return println("The remaining cards are insufficient to meet the request.")
+            else -> {
+                println(this.cards.slice(0 until n).joinToString(" "))
+                this.cards = this.cards.slice(n .. this.cards.lastIndex)
+            }
+        }
+    }
+}
+
+class Game {
+    private val deck = Deck()
+
+    fun promptAction() {
+        println("Choose an action (reset, shuffle, get, exit):")
+
+        val action = readln()
+        if (action == "exit") return println("Bye")
+        when (action) {
+            "reset" -> this.deck.reset()
+            "shuffle" -> this.deck.shuffle()
+            "get" -> {
+                println("Number of cards:")
+                this.deck.get(readln())
+            }
+            else -> println("Wrong action.")
+        }
+
+        promptAction()
+    }
+}
 
 fun main() {
-    println(RANKS.joinToString(" "))
-    println(SUITS.joinToString(" "))
-    println(DECK.joinToString(" "))
+    val game = Game()
+    game.promptAction()
 }

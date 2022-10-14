a4830b231dc2d68edd9aa3d442d8743a5a83f5dd
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-10-14 23:09:58 +0200

    Add topic "Object declarations"

---
+++ Simple Search Engine/Topics/Object declarations/Blocks in a box/src/Main.kt
@@ -0,0 +1,9 @@
+class Block(val color: String) {
+    object BlockProperties {
+        var length = 6
+        var width = 4
+
+        fun blocksInBox(length: Int, width: Int) =
+            length / this.length * (width / this.width)
+    }
+}
---
+++ Simple Search Engine/Topics/Object declarations/Library/src/Main.kt
@@ -0,0 +1,4 @@
+object Math {
+    fun abs(i: Int) = if (i < 0) -i else i
+    fun abs(d: Double) = if (d < 0) -d else d
+}

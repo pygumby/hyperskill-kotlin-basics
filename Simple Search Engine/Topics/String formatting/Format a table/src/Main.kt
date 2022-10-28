deab7b28a4bf152e2e740a79c9d3224ed1ea5cfc
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-10-28 21:02:05 +0200

    Add topic "String formatting"

---
+++ Simple Search Engine/Topics/String formatting/Format a table/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val a = readln().toDouble()
+    val b = readln().toDouble()
+    val c = readln().toDouble()
+    val d = readln().toDouble()
+
+    println(String.format("%,12.2f%,12.2f", a, b))
+    println(String.format("%,12.2f%,12.2f", c, d))
+}

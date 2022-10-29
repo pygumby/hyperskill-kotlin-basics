a24c4526e0cb08cba561c7c2e5d48fd23bdc1fc6
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-10-29 19:17:48 +0200

    Add topic "Set"

---
+++ Simple Search Engine/Topics/Set/Combination/src/Main.kt
@@ -0,0 +1,2 @@
+fun solution(first: Set<String>, second: Set<String>): Set<String> =
+    first + second
---
+++ Simple Search Engine/Topics/Set/Element problem/src/Main.kt
@@ -0,0 +1,2 @@
+fun solution(first: Set<String>, second: MutableList<String>): Boolean =
+    first == second.toSet()

4b00e94c4c5328b66486a779d28b6a23d54cbeb3
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2022-10-31 12:23:17 +0100

    Add topic "Multi-dimensional array"

---
+++ Simple Search Engine/Topics/Multi-dimensional array/Cube of zeros/src/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    println(
+        arrayOf(
+            arrayOf(
+                arrayOf(0, 0, 0),
+                arrayOf(0, 0, 0),
+                arrayOf(0, 0, 0),
+            ),
+            arrayOf(
+                arrayOf(0, 0, 0),
+                arrayOf(0, 0, 0),
+                arrayOf(0, 0, 0),
+            ),
+            arrayOf(
+                arrayOf(0, 0, 0),
+                arrayOf(0, 0, 0),
+                arrayOf(0, 0, 0),
+            )
+        ).contentDeepToString()
+    )
+}
---
+++ Simple Search Engine/Topics/Multi-dimensional array/Shapeshifter/src/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    //Do not touch code below
+    var inputArray: Array<Array<String>> = arrayOf()
+    val n = readLine()!!.toInt()
+    for (i in 0 until n) {
+        val strings = readLine()!!.split(' ').toTypedArray()
+        inputArray += strings
+    }
+    // write your code here    
+    print("[")
+    print(inputArray.last().contentToString())
+    print(", ")
+    print(inputArray.first().contentToString())
+    print("]")
+}
